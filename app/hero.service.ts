e4d7405abea82064c7be42c258464bab0090f7a4
Adrian Lemes <adrianlemess@gmail.com>
2016-06-17 17:32:00 -0300

    Angular routes

--- app/hero.service.ts
+++ app/hero.service.ts
@@ -5,9 +5,4 @@ export class HeroService {
     getHeroes() {
         return Promise.resolve(HEROES);
     }
-    getHeroesSlowly() {
-        return new Promise<Hero[]>(resolve =>
-            setTimeout(() => resolve(HEROES), 2000) // 2 seconds
-        );
-    }
 }
